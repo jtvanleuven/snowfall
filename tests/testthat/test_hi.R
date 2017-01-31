ac23573944899cac14eb7686fa773ac3a0e26a48
jtvanleuven <jtvanleuven@gmail.com>
2017-01-31 12:01:12 -0800

    added vignette

--- tests/testthat/test_hi.R
+++ tests/testthat/test_hi.R
@@ -11,3 +11,12 @@ test_that("is_scalar_character() works", {
   expect_false(is_scalar_character(c("foo","bar")))
   expect_false(is_scalar_character(NA_character_))
 })
+
+test_that("hi() only accepts appropriate 'how'", {
+  expect_equal(hi("jt","shout"),"hi JT how are you?")
+  expect_equal(hi("jt","whisper"),"hi jt how are you?")
+  expect_equal(hi("Jt","asis"),"hi Jt how are you?")
+  expect_error(hi(123,123))
+  expect_error(hi("",""))
+  expect_error(hi(NA_character_,NA_character_))
+})
